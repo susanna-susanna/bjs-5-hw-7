b6005da4930e83e4f4af47bf245404d1f30f9d78
Susanna Alieva <susanna_susanna_00@mail.ru>
2019-06-13 12:38:25 +0300

    Redid task according teacher's remarks

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
+function compareArrays(arr1, arr2) {
+  return arr1.length !== arr2.length ? false : arr1.every((element, i) => element === arr2[i]);
+}
+
+/* OLD SOLUTION
+
 function compareArrays( arr1, arr2 ) {
   return (JSON.stringify(arr1) == JSON.stringify(arr2));
 }
+*/
 
 function sum1(...args) {
   let sum = 0;
@@ -11,6 +18,23 @@ function sum1(...args) {
 }
 
 const mSum = memorize(sum1, 5);
+
+function memorize(fn, limit) {
+  let results = [];
+  return function () {
+    check = results.find(result => compareArrays(result.args, Array.from(arguments)));
+    if (!check) {
+results.push({args: Array.from(arguments), result: fn(...arguments)})
+      if (results.length > limit) {
+        results.pop();
+      }
+      let i = results.length - 1;
+      return results[i].result;
+    } 
+  }
+}
+
+/* OLD SOLUTION
 
 function memorize(fn, limit) {
   let results = [];
@@ -28,6 +52,13 @@ function memorize(fn, limit) {
     }
   }
 }
+*/
+
+console.log(compareArrays([8, 9], [6])); 
+compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5]); 
+compareArrays([9, 2, 4, 8, 2], [9, 2, 4]); 
+console.log(compareArrays([1, 2, 3], [2, 3, 1])); 
+console.log(compareArrays([8, 1, 2], [8, 1, 2])); 
 
 console.log(mSum(1, 2, 3));
 console.log(mSum(4, 400));
